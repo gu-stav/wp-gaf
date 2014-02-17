734c602243d8bb51d7b0bb6731263eb15f2c6407
Gustav Pursche <gustavpursche@gmail.com>
2014-02-17 17:10:01 +0100

    cleanup category.php

--- category.php
+++ category.php
@@ -5,14 +5,6 @@
 <section class="content">
   <?php
     if ( have_posts() ) : while ( have_posts() ) : the_post();
-
-    $background = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-
-    if( $background[0] ) {
-      $background = $background[0];
-    } else {
-      $background = '';
-    }
   ?>
 
   <?php get_template_part( 'post' ); ?>
