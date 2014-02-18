93ee4f0b81d2cebd4b56c7cb4a06e88f6ff8562d
Gustav Pursche <gustavpursche@gmail.com>
2014-02-19 00:22:15 +0100

    show upcoming exhibitions below the current one, introduce area divider class. closes #7,#9

--- index.php
+++ index.php
@@ -7,6 +7,24 @@
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php get_template_part( 'post' ); ?>
   <?php endwhile; endif; ?>
+
+<?php
+  query_posts( $query_string . '&category_name=past' );
+?>
+
+<?php
+  if ( have_posts() ) {
+?>
+  <h3 class="content__category-divider">&nbsp;Upcoming</h3>
+<?php
+  }
+?>
+
+  <div id="upcoming">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part( 'post' ); ?>
+    <?php endwhile; endif; ?>
+  </div>
 </section>
 
 <?php get_footer(); ?>
